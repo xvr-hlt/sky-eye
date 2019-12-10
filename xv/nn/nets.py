d538cb4c705b7052189763a68485f5c249611abc
Xavier <xavier@sypht.com>
2019-12-10 00:11:05 +0000

    added BIFPN

--- xv/nn/nets.py
+++ xv/nn/nets.py
@@ -6,41 +6,57 @@ import segmentation_models_pytorch as smp
 from torchvision.models.detection.faster_rcnn import TwoMLPHead
 from torchvision.ops.feature_pyramid_network import FeaturePyramidNetwork, LastLevelMaxPool
 from collections import OrderedDict
+from xv.nn.bifpn import BIFPN
 
 
 class BoxClassifier(nn.Module):
-    def __init__(self, encoder_name, nclasses=4, features=(4,3,2,1), out_channels=256):
+    def __init__(self, encoder_name,
+                 nclasses=4,
+                 use_bifpn=False,
+                 features=(4,3,2,1),
+                 out_channels=256, 
+                 dropout=.1,
+                 representation_size=1024):
         super().__init__()
         self.encoder = smp.encoders.get_encoder(encoder_name, 'imagenet')
         self.features = features
         
+        self.use_bifpn = use_bifpn
+        
         out_shapes = smp.encoders.encoders[encoder_name]['out_shapes']
-        self.fpn = FeaturePyramidNetwork([out_shapes[f] for f in features],
-                                         out_channels,
-                                         extra_blocks=LastLevelMaxPool())
-
+        out_shapes = [out_shapes[f] for f in features]
+        
+        if not self.use_bifpn:
+            self.fpn = FeaturePyramidNetwork(out_shapes,
+                                             out_channels,
+                                             extra_blocks=LastLevelMaxPool())
+        else:
+            self.fpn = BIFPN(out_shapes, out_channels, num_outs=len(features))
+        
         self.roi_pool = MultiScaleRoIAlign(
             featmap_names=features,
             output_size=7,
             sampling_ratio=2
         )
-
+        
+        self.dropout = nn.Dropout2d(p=dropout)
+        
         resolution = self.roi_pool.output_size[0]
-        representation_size = 1024
+        
         self.box_head = TwoMLPHead(out_channels * resolution ** 2, representation_size)
         self.head = nn.Linear(representation_size, nclasses)
 
     def forward(self, x, boxes):
         i, _, w, h = x.shape
         sizes = [(w, h) for _ in range(i)]
-        
         x = self.encoder(x)
-        x = OrderedDict((i, x[i]) for i in self.features)
+        x = [x[i] for i in self.features] if self.use_bifpn else OrderedDict((i, x[i]) for i in self.features)
         x = self.fpn(x)
+        if self.use_bifpn: x = OrderedDict((ix, xi) for ix, xi in enumerate(x))
         x = self.roi_pool(x, boxes, sizes)
+        x = self.dropout(x)
         x = self.box_head(x)
         x = self.head(x)
-        
         return x
 
 
