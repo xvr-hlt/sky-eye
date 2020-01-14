aa52ea6890a7ff8ff7f3e9f12bdf9a5b578cc687
DeepLearning VM <google-dl-platform@googlegroups.com>
2020-01-14 04:54:14 +0000

    docker push xvrhlt/sky-eye:lite

--- xv/nn/__init__.py
+++ xv/nn/__init__.py
@@ -6,12 +6,12 @@ def get_model_config(*ims):
     return [
         {
             'localization_kwargs': {
-                'run_ids': ['8bh5rurv', 'cnqyhmsb', 'ri1dvvam', 'ilhz9vkq'],
+                'run_ids': ['8bh5rurv'],
                 'thresh': 0.2
             },
             'damage_kwargs': {
-                'damseg_ids': ['tyi7q17m','ngmxec6s', 'vrgbzaup', 'xei17o3b', '10gtu54s'],#'443t9312', 'pxcuoulb'],#
-                'damod_ids': ['kx9yzcb9', 'y7n4yzya', 'c1lit0gf'],
+                'damseg_ids': ['tyi7q17m'],#'443t9312', 'pxcuoulb'],#
+                'damod_ids': [],
                 'seg_mean_ag': False
             }
         },
