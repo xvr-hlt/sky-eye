d69e70f10a300fcc71a3ebbb87e38490b4e21861
DeepLearning VM <google-dl-platform@googlegroups.com>
2020-01-15 11:45:27 +0000

    added conf file

---
+++ config.py
@@ -0,0 +1,151 @@
+{
+    "release/qoijsx0h_0gvvydkt": [
+        {
+            "localization_kwargs": {
+                "run_ids": ["8bh5rurv", "cnqyhmsb", "ri1dvvam", "ilhz9vkq"],
+                "thresh": 0.2,
+            },
+            "damage_kwargs": {
+                "damseg_ids": [
+                    "tyi7q17m",
+                    "ngmxec6s",
+                    "vrgbzaup",
+                    "443t9312",
+                    "pxcuoulb",
+                ],
+                "damod_ids": ["kx9yzcb9", "y7n4yzya", "c1lit0gf"],
+                "seg_mean_ag":
+                False,
+            },
+        },
+        {
+            "localization_kwargs": {
+                "run_ids": ["qoijsx0h"],
+                "thresh": 0.5
+            },
+            "damage_kwargs": {
+                "damseg_ids": ["0gvvydkt"],
+                "damod_ids": [],
+                "seg_mean_ag": True,
+            },
+        },
+    ],
+    "qoijsx0h_kx9yzcb9_1": [
+        {
+            "localization_kwargs": {
+                "run_ids": ["8bh5rurv", "cnqyhmsb", "ri1dvvam", "ilhz9vkq"],
+                "thresh": 0.2,
+            },
+            "damage_kwargs": {
+                "damseg_ids": [
+                    "tyi7q17m",
+                    "ngmxec6s",
+                    "vrgbzaup",
+                    "443t9312",
+                    "pxcuoulb",
+                ],
+                "damod_ids": ["kx9yzcb9", "y7n4yzya", "c1lit0gf"],
+                "seg_mean_ag":
+                True,
+            },
+        },
+        {
+            "localization_kwargs": {
+                "run_ids": ["qoijsx0h"],
+                "thresh": 0.5
+            },
+            "damage_kwargs": {
+                "damseg_ids": ["0gvvydkt"],
+                "damod_ids": [],
+                "seg_mean_ag": True,
+            },
+        },
+    ],
+    "lite": [
+        {
+            "localization_kwargs": {
+                "run_ids": ["8bh5rurv"],
+                "thresh": 0.2
+            },
+            "damage_kwargs": {
+                "damseg_ids": ["tyi7q17m"],
+                "damod_ids": [],
+                "seg_mean_ag": False,
+            },
+        },
+        {
+            "localization_kwargs": {
+                "run_ids": ["qoijsx0h"],
+                "thresh": 0.5
+            },
+            "damage_kwargs": {
+                "damseg_ids": ["hsxmom00"],
+                "damod_ids": [],
+                "seg_mean_ag": True,
+            },
+        },
+    ],
+    "qoijsx0h_hsxmom00_1": [
+        {
+            "localization_kwargs": {
+                "run_ids": ["8bh5rurv", "cnqyhmsb", "ri1dvvam", "ilhz9vkq"],
+                "thresh": 0.2,
+            },
+            "damage_kwargs": {
+                "damseg_ids": [
+                    "tyi7q17m",
+                    "ngmxec6s",
+                    "vrgbzaup",
+                    "xei17o3b",
+                    "10gtu54s",
+                ],
+                "damod_ids": ["kx9yzcb9", "y7n4yzya", "c1lit0gf"],
+                "seg_mean_ag":
+                False,
+            },
+        },
+        {
+            "localization_kwargs": {
+                "run_ids": ["qoijsx0h"],
+                "thresh": 0.5
+            },
+            "damage_kwargs": {
+                "damseg_ids": ["hsxmom00"],
+                "damod_ids": [],
+                "seg_mean_ag": True,
+            },
+        },
+    ],
+    "qoijsx0h_ngmxec6s": [
+        {
+            "localization_kwargs": {
+                "run_ids": ["8bh5rurv", "cnqyhmsb", "ri1dvvam", "ilhz9vkq"],
+                "thresh": 0.2,
+            },
+            "damage_kwargs": {
+                "damseg_ids": [
+                    "tyi7q17m",
+                    "ngmxec6s",
+                    "vrgbzaup",
+                    "xei17o3b",
+                    "10gtu54s",
+                ],
+                "damod_ids": ["kx9yzcb9", "y7n4yzya", "c1lit0gf"],
+                "seg_mean_ag":
+                True,
+            },
+        },
+        {
+            "localization_kwargs": {
+                "run_ids": ["qoijsx0h"],
+                "thresh": 0.5
+            },
+            "damage_kwargs": {
+                "damseg_ids": ["ngmxec6s"],
+                "damod_ids": [],
+                "seg_mean_ag": True,
+            },
+        },
+    ],
+}
+
